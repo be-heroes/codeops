10319d2177eb65448b013a7a768ffd5b767f4963
ZaradarBH <tobias.andersen79@gmail.com>
2023-04-28 18:27:48 +0200

    Add IKeyPair.Name

--- src/BeHeroes.CodeOps.Abstractions/Cryptography/IKeyPair.cs
+++ src/BeHeroes.CodeOps.Abstractions/Cryptography/IKeyPair.cs
@@ -2,6 +2,8 @@ namespace BeHeroes.CodeOps.Abstractions.Cryptography
 {
     public interface IKeyPair
     {
+        string Name { get; }
+
         PrivateKey Private{ get; }
 
         PublicKey Public { get; }
