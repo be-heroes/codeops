79bfcab537881357132770ce0fd9fe7769bb851e
ZaradarBH <tobias.andersen79@gmail.com>
2023-04-28 16:57:02 +0200

    defer key initialization to derived types

--- src/BeHeroes.CodeOps.Abstractions/Encryption/Key.cs
+++ src/BeHeroes.CodeOps.Abstractions/Encryption/Key.cs
@@ -5,10 +5,6 @@ namespace BeHeroes.CodeOps.Abstractions.Encryption
 {
     public abstract class Key : SecurityKey, IValidatableObject
     {
-        protected byte[]? _rawData;
-
-        public override int KeySize => _rawData?.Length ?? 0;
-
         public abstract IEnumerable<ValidationResult> Validate(ValidationContext validationContext);
     }
 }
