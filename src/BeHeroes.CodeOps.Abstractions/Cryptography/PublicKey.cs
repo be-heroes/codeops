4aef28ce5da12e64c4cd7294a9e1f738eda8c92f
ZaradarBH <tobias.andersen79@gmail.com>
2023-04-28 17:33:05 +0200

    - Pruning NuGet dependency graph - Bumping Priv / Pub key abstractions to code ops

---
+++ src/BeHeroes.CodeOps.Abstractions/Cryptography/PublicKey.cs
@@ -0,0 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+
+using ProtoBuf;
+
+namespace BeHeroes.CodeOps.Abstractions.Cryptography
+{
+    [ProtoContract]
+    public abstract class PublicKey : Key
+    {
+        [ProtoMember(1)]
+        public override string KeyId {
+            get {
+                return base.KeyId;
+            }
+            set{
+                base.KeyId = value;
+            }
+        }
+
+        [ProtoMember(2)]
+        public byte[]? KeyData
+        {
+            get; init;
+        }
+                
+        public override int KeySize => KeyData?.Length ?? 0;
+
+        public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {   
+            if(KeyData == null || KeySize == 0)
+                yield return new ValidationResult("KeyData is not initialized");
+
+            if(string.IsNullOrEmpty(KeyId))
+                yield return new ValidationResult("KeyId is not initialized");
+        }
+    }
+}
