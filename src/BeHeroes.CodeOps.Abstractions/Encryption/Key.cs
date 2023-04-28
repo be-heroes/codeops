b1b2d7051007b47733445c723b44ba0892e2bf3c
ZaradarBH <tobias.andersen79@gmail.com>
2023-04-28 05:25:25 +0200

    simplify key abstraction

--- src/BeHeroes.CodeOps.Abstractions/Encryption/Key.cs
+++ src/BeHeroes.CodeOps.Abstractions/Encryption/Key.cs
@@ -4,15 +4,7 @@ namespace BeHeroes.CodeOps.Abstractions.Encryption
 {
     public abstract class Key : SignatureProvider, IValidatableObject
     {
-        public byte[] Data {get; init;}
-
-        public string? Identifier {get; init;}
-
-        protected Key(byte[] rawData, string? identifier)
-        {
-            Data = rawData;
-            Identifier = identifier;
-        }
+        protected byte[]? _rawData;
 
         public abstract IEnumerable<ValidationResult> Validate(ValidationContext validationContext);
     }
