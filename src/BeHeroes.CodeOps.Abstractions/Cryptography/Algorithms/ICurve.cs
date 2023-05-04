513ad82bdc72d402c1876d35dd3425983715c74b
ZaradarBH <tobias.andersen79@gmail.com>
2023-05-04 17:31:20 +0200

    Added convenience logic to ICurve interface

--- src/BeHeroes.CodeOps.Abstractions/Cryptography/Algorithms/ICurve.cs
+++ src/BeHeroes.CodeOps.Abstractions/Cryptography/Algorithms/ICurve.cs
@@ -3,5 +3,21 @@ namespace BeHeroes.CodeOps.Abstractions.Cryptography.Algorithms
     public interface ICurve
     { 
         string Identifier { get; }
+
+        int CoordinateSystem { get; }
+        
+        int FieldSize { get; }
+        
+        byte[] Cofactor { get; }
+        
+        byte[] Order { get; }
+
+        byte[] GetSeed();
+
+        bool SupportsCoordinateSystem(int coord);
+
+        bool Equals(ICurve other);
+
+        int GetHashCode();
     }
 }
