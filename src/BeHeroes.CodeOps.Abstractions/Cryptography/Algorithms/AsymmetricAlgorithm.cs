49d98e82daf0565d6da051cf4cfb41d1b35864c3
ZaradarBH <tobias.andersen79@gmail.com>
2023-05-04 16:42:29 +0200

    Revert to interfaces due to legacy factory logic in dotnet

--- src/BeHeroes.CodeOps.Abstractions/Cryptography/Algorithms/AsymmetricAlgorithm.cs
+++
@@ -1,9 +0,0 @@
-namespace BeHeroes.CodeOps.Abstractions.Cryptography.Algorithms
-{
-    public abstract class AsymmetricAlgorithm : Algorithm, IAsymmetricAlgorithm
-    {
-        public string? KeyExchangeAlgorithm { get; init ; }
-
-        public string? SignatureAlgorithm { get; init ; }
-    }
-}
