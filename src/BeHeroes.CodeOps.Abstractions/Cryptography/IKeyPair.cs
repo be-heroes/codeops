4a8e9f76bd8410904b1f2fec858d889ff1b93f86
ZaradarBH <tobias.andersen79@gmail.com>
2023-05-04 04:43:05 +0200

    Added Algorithm property to IKeyPair interface

--- src/BeHeroes.CodeOps.Abstractions/Cryptography/IKeyPair.cs
+++ src/BeHeroes.CodeOps.Abstractions/Cryptography/IKeyPair.cs
@@ -1,7 +1,11 @@
+using BeHeroes.CodeOps.Abstractions.Cryptography.Algorithms;
+
 namespace BeHeroes.CodeOps.Abstractions.Cryptography
 {
     public interface IKeyPair
     {
+        IAlgorithm Algorithm { get; }
+
         Key Private{ get; }
 
         Key Public { get; }
