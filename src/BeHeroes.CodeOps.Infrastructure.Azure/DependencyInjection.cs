57ba30b7e2567bfb47dabe1d3ef178482467f197
ZaradarBH <tobias.andersen79@gmail.com>
2023-05-08 08:15:55 +0200

    Initial broker

---
+++ src/BeHeroes.CodeOps.Infrastructure.Azure/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BeHeroes.CodeOps.Infrastructure.Azure
+{
+    public static class DependencyInjection
+    {
+        public static void AddAzure(this IServiceCollection services, IConfiguration configuration)
+        {
+        }
+    }
+}
