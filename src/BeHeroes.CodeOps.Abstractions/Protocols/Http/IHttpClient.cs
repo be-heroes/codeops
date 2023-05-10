9982ad406b609a619f3b2fc9c851f827936385ee
ZaradarBH <tobias.andersen79@gmail.com>
2023-05-10 17:11:09 +0200

    Change to interface based strategy, the RestClient abstraction is pointless if based on HttpClient

--- src/BeHeroes.CodeOps.Abstractions/Protocols/Http/IHttpClient.cs
+++ src/BeHeroes.CodeOps.Abstractions/Protocols/Http/IHttpClient.cs
@@ -1,7 +1,9 @@
 ﻿namespace BeHeroes.CodeOps.Abstractions.Protocols.Http
 {
-    public interface IRestClient : IDisposable, IAsyncDisposable
+    public interface IHttpClient : IDisposable, IAsyncDisposable
     {
+        IEnumerable<IHttpHeader> DefaultRequestHeaders { get; }
+
         Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken);
     }
 }
---
+++ src/BeHeroes.CodeOps.Abstractions/Protocols/Http/IHttpHeader.cs
@@ -0,0 +1,9 @@
+﻿namespace BeHeroes.CodeOps.Abstractions.Protocols.Http
+{
+    public interface IHttpHeader
+    {
+        string Name { get; }
+
+        IEnumerable<string> Values { get; }
+    }
+}
